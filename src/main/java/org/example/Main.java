7ece273c1ff1054990b54faf2c937950ce8c1e68
WJijon223 <williamjijon223@gmail.com>
2025-03-17 13:09:11 -0400

    implemented storeData() method: stores WeatherData instances inside ArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String file = "src/main/resources/data/weatherdata.csv";
+        ArrayList<WeatherData> weatherData = new ArrayList<WeatherData>();
+        storeData(file, weatherData);
     }
+
+    public static void storeData(String filePath, ArrayList<WeatherData> weatherdata) {
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine()) !=  null) {
+                String[] row = line.split(",");
+                weatherdata.add(new WeatherData(row[0], Double.parseDouble(row[1]), Double.parseDouble(row[2]), Double.parseDouble(row[3])));
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
+
+record WeatherData(String date, double temperature, double Humidity, double precipitation) {}
