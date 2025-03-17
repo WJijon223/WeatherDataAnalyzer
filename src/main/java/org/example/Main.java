932a38ecc496fa54ec38c4223c072038caa8a4c6
WJijon223 <williamjijon223@gmail.com>
2025-03-17 13:45:34 -0400

    implemented printAverageTemp() method which finds average temperature for a given month

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,43 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String file = "src/main/resources/data/weatherdata.csv";
         ArrayList<WeatherData> weatherData = new ArrayList<WeatherData>();
         storeData(file, weatherData);
-        printData(weatherData);
+        printAverageTemp(01, weatherData);
     }
+
+    public static void printData(ArrayList<WeatherData> weatherdata) {
+        for (WeatherData data : weatherdata) {
+            System.out.println(data);
+        }
+    }
+
+    public static void printAverageTemp(int month, ArrayList<WeatherData> weatherdata) {
+        if (month < 1 || month > 12) {
+            System.out.println("Invalid month number. Please enter a number between 1 and 12.");
+            return;
+        }
+
+        List<WeatherData> filteredData;
+        filteredData = weatherdata.stream()
+                .filter(data -> Integer.parseInt(data.date().substring(5, 7)) == month)
+                .toList();
+
+        double totalTemp = filteredData.stream().mapToDouble(WeatherData::temperature).sum();
+        double averageTemp = totalTemp / filteredData.size();
+        System.out.printf("Average temperature for month " + month + ": %.2f\n", averageTemp);
+    }
+
+    //TODO: Created a method to find days that meet the minimum temperature for a given set of data
+    public static void findMinTempDays(double weatherMin, ArrayList<WeatherData> weatherdata) {}
+
+    //TODO: Create a method to find and print the number of rainy days in a given set of data
+    public static void findNumRainyDays(ArrayList<WeatherData> weatherdata){}
 
     public static void storeData(String filePath, ArrayList<WeatherData> weatherdata) {
         BufferedReader reader = null;
@@ -32,21 +61,6 @@ public class Main {
             }
         }
     }
-
-    public static void printData(ArrayList<WeatherData> weatherdata) {
-        for (WeatherData data : weatherdata) {
-            System.out.println(data);
-        }
-    }
-
-    //TODO: Create a method to find and print average temperature for a given month
-    public static void printAverageTemp(String month, ArrayList<WeatherData> weatherdata) {}
-
-    //TODO: Created a method to find days that meet the minimum temperature for a given set of data
-    public static void findMinTempDays(double weatherMin, ArrayList<WeatherData> weatherdata) {}
-
-    //TODO: Create a method to find and print the number of rainy days in a given set of data
-    public static void findNumRainyDays(ArrayList<WeatherData> weatherdata){}
 }
 
 record WeatherData(String date, double temperature, double Humidity, double precipitation) {}
